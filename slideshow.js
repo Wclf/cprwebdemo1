20bd4ec62e169510d20e1fbf5571df260fb1c134
Walnut113 <thang10102003@gmail.com>
2023-01-26 12:08:35 +0700

    update2

--- slideshow.js
+++ slideshow.js
@@ -4,7 +4,9 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 // next and previous controls
-
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 // thumbnails images controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
@@ -13,12 +15,18 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for( i = 0; i < slides.length; i++ ) {
         slides[i].style.display = "none";
     }
 
+    for(i = 0; i < dots.length;i++) {
+        dots[i].className = dots[i].className.replace("  active", "");
+    }
+
     slides[slideIndex-1].style.display = "block";
-    
+    dots[slideIndex-1].className += "  active";
 }
+
